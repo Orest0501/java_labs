a10d21e96de29a40160e1191770a0b73ede9d98c
Orest0501 <oborshovskiy@gmail.com>
2022-05-17 17:52:45 +0300

    lab2-4 was tested
    Lab was tested with coverage - 84%

---
+++ src/main/java/ua/lviv/iot/models/FootballStadium.java
@@ -0,0 +1,44 @@
+package ua.lviv.iot.models;
+
+public class FootballStadium extends SportFacilities {
+
+    private final String stadiumColour;
+
+    public FootballStadium(int capacity, String name, String typeOfGround, String typeOfSport, String stadiumСolour) {
+
+        super(capacity, name, typeOfGround, typeOfSport);
+
+        this.capacity = capacity;
+
+        this.name = name;
+
+        this.typeOfGround = typeOfGround;
+
+        this.typeOfSport = typeOfSport;
+
+        this.stadiumColour = "red-black";
+
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
+
+    public String getTypeOfSport() {
+        return typeOfSport;
+    }
+
+    public String getStadiumColour() {
+        return stadiumColour;
+    }
+
+    @Override
+    public String toString() {
+        return "FootballStadium{" +
+                "stadiumColour='" + stadiumColour + '\'' +
+                ", capacity=" + capacity +
+                ", name='" + name + '\'' +
+                ", typeOfGround='" + typeOfGround + '\'' +
+                ", typeOfSport='" + typeOfSport + '\'' +
+                '}';
+    }}
---
+++ src/test/java/ua/lviv/iot/manager/ManagerTest.java
@@ -0,0 +1,57 @@
+package ua.lviv.iot.manager;
+
+import org.junit.jupiter.api.Test;
+import ua.lviv.iot.models.BasketballArena;
+import ua.lviv.iot.models.FootballStadium;
+import ua.lviv.iot.models.IcyArena;
+import ua.lviv.iot.models.SportFacilities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagerTest {
+
+        Manager man = new Manager();
+
+        List<SportFacilities> sport1 = man.sortingByTypes(man.getList());
+
+        String sport1param0 = "Athletics";
+        String sport1param1 = "Curling";
+        String sport1param2 = "Volleyball";
+
+        List<SportFacilities> sport2 = man.sortingByTypesReversed(man.getList());
+
+        String sport2param0 = "Volleyball";
+        String sport2param1 = "Curling";
+        String sport2param2 = "Athletics";
+
+        List<SportFacilities> sport3 = man.SearchByCapacity(man.getList());
+
+        int capacity1 = 800;
+        int capacity2 = 498;
+        int capacity3 = 998;
+
+        @Test
+        void sortingByTypes() {
+            assertEquals(sport1param0, sport1.get(0).getTypeOfSport());
+            assertEquals(sport1param1, sport1.get(1).getTypeOfSport());
+            assertEquals(sport1param2, sport1.get(2).getTypeOfSport());
+        }
+
+        @Test
+        void sortingByTypesReversed () {
+            assertEquals(sport2param0, sport2.get(0).getTypeOfSport());
+            assertEquals(sport2param1, sport2.get(1).getTypeOfSport());
+            assertEquals(sport2param2, sport2.get(2).getTypeOfSport());
+        }
+
+        @Test
+        void searchByCapacity () {
+            assertEquals(capacity1, sport2.get(0).getCapacity());
+            assertEquals(capacity2, sport2.get(1).getCapacity());
+            assertEquals(capacity3, sport2.get(2).getCapacity());
+        }
+
+    }
