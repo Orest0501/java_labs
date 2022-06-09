872b097a2f7c9e78b49d01e2b27eddafcad4bb5c
Orest0501 <oborshovskiy@gmail.com>
2022-06-09 12:19:48 +0300

    lab 6 was edited

--- src/main/java/ua/lviv/iot/manager/FacilityWriter.java
+++ src/main/java/ua/lviv/iot/manager/FacilityWriter.java
@@ -11,11 +11,9 @@ class FacilityWriter {
 
     public void writeToFile() throws IOException {
 
-
         BasketballArena volleyballArena2 = new BasketballArena(800, "Prometey arena", "Floor", "Volleyball", "Bucha");
         IcyArena icyArena = new IcyArena(19280, "Ternopil Ice", "Ice","Hockey", 8 );
         FootballStadium footballStadium = new FootballStadium(68500, "Mykolaiv Stadium", "Grass", "Football", "red-black");
-
 
         BufferedWriter writer = new BufferedWriter(new FileWriter("arenas.csv"));
         writer.write(volleyballArena2.getHeaders());
@@ -23,18 +21,18 @@ class FacilityWriter {
         writer.write(volleyballArena2.toCSV());
         writer.close();
 
-
         BufferedWriter writer1 = new BufferedWriter(new FileWriter("arenas1.csv"));
         writer1.write(footballStadium.getHeaders());
         writer1.write("\r\n");
         writer1.write(footballStadium.toCSV());
         writer1.close();
 
-
         BufferedWriter writer2 = new BufferedWriter(new FileWriter("arenas2.csv"));
         writer2.write(icyArena.getHeaders());
         writer2.write("\r\n");
         writer2.write(icyArena.toCSV());
         writer2.close();
+
+
 
 }}
